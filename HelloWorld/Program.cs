79c31c45ea43485ab668da44475f5981d4c0a2b4
Kevin Winn <Kevin Winn@DESKTOP-SUU8ESM>
2016-11-22 15:53:46 -0700

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+}
+    
