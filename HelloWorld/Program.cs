93960e62392e3208fb673246773203b96ef93ce7
Kevin Winn <Kevin Winn@DESKTOP-SUU8ESM>
2016-11-29 16:53:58 -0700

    testing agianst static method for MSTest

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,18 +1,24 @@
 ﻿//using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NUnit.Framework;
+
 
 namespace HelloWorld
 {
     class Program
     {
+        static int addTwoThings(int thingOne, int thingTwo)
+        {
+            return thingOne + thingTwo;
+        }
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello, World!");
+            System.Console.WriteLine(addTwoThings(1, 4));
+            System.Console.WriteLine("Hello World");
         }
     }
 }
-    
+
